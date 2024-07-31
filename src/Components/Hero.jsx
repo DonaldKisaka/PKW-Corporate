208850924ea7df6dacc88c78051d3cf5a03ec054
Donald Kisaka <kisakadonald1@gmail.com>
2024-07-31 11:25:54 +0300

    Initial commit

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -12,7 +12,8 @@ const Hero = () => {
             <img src={image} alt="image" className="z-[2] absolute left-0 w-1/3 pl-10 mt-20 pt-10 max-md:hidden" />
             <div className="w-full min-h-screen flex flex-col items-center  relative max-md:justify-center max-md:w-auto max-md:items-center max-md:text-center justify-center  ml-64 max-md:ml-0 -mt-50">
               <h1 className="text-2xl text-slate-600 font-montserrat font-bold">Philip K. Wafula CPA(K) MBA</h1>
-              <p className="text-xl text-slate-500 font-bold">Accounting Consultant</p>
+              <p className="text-xl
+               text-slate-500 font-bold">Accounting Consultant</p>
               <p className="font-montserrat text-slate-gray leading-normal text-xl mt-10">Tax, Accounting, Bookkeeping, Payroll Administration & more.</p>
               <Button label="Book a Consultation" />
             </div>
