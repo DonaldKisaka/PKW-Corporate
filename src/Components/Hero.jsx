fc2b3bdb48c8bdccda0ad040fb43cadafc2e0ded
DonaldKisaka <kisakadonald1@gmail.com>
2024-08-28 15:16:08 +0300

    I have changed some things again for improvement and again

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
     <>
       <section id="home" className="bg-hero w-full min-h-screen h-auto flex flex-col md:flex-row container max-container padding-x relative">
         <div className="container relative flex flex-col items-center md:items-center md:flex-row">
-            <img src={img9} alt="image" className="z-[2] w-1/2  md:w-1/4 md:pl-10  md:pt-10 max-md:pt-32" />
+            <img src={img8} alt="image" className="z-[2] w-1/2  md:w-1/4 md:pl-10  md:pt-10 max-md:pt-32" />
             <div className="w-full flex flex-col items-center justify-center md:justify-start text-center md:text-left md:ml-10 mt-5 md:mt-0">
               <h1 className="text-2xl md:text-3xl text-slate-600 font-montserrat font-bold max-md:text-xl">Philip K. Wafula CPA(K) MBA</h1>
               <p className="text-xl
